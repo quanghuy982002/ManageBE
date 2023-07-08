21b68021f217d6950ee02613777f20e5b4fb3def
Luu Quang Huy <86486022+quanghuy982002@users.noreply.github.com>
2023-07-08 09:27:20 +0700

    fist commit

---
+++ src/main/java/com/viettel/managebe/ManageBeApplication.java
@@ -0,0 +1,13 @@
+package com.viettel.managebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManageBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/viettel/managebe/ManageBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viettel.managebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
